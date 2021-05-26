050049a5ab79948069d764b922901f75a883eabe
Yuxin Zou <12762985+davidzyx@users.noreply.github.com>
2021-05-26 10:00:04 -0700

    chrome crash fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_setup_options():
     options = Options()
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-extensions')
     options.add_argument('--disable-dev-shm-usage')
